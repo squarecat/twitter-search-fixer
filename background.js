3ad9b142f50a7a41efd9a2f8699b556943d2e251
Danielle Johnson <dinkydani@gmail.com>
2019-01-06 12:01:01 -0500

    fix: options popup not showing in chrome after ff version

--- background.js
+++ background.js
@@ -8,13 +8,13 @@ const options = {
   twitterFixerSettingHide: true
 };
 
-chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
-  if (changeInfo.status === 'complete' && tab.url.match(/twitter\.com/)) {
-    chrome.pageAction.show(tabId);
-  } else {
-    chrome.pageAction.hide(tabId);
-  }
-});
+// chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
+//   if (changeInfo.status === 'complete' && tab.url.match(/twitter\.com/)) {
+//     chrome.pageAction.show(tabId);
+//   } else {
+//     chrome.pageAction.hide(tabId);
+//   }
+// });
 
 chrome.runtime.onInstalled.addListener(function() {
   chrome.storage.sync.set(options);
@@ -32,3 +32,19 @@ chrome.runtime.onInstalled.addListener(function() {
   //   ]);
   // });
 });
+
+function onWebNav(details) {
+  if (details.frameId === 0) {
+    // Top-level frame
+    chrome.pageAction.show(details.tabId);
+  }
+}
+const filter = {
+  url: [
+    {
+      hostEquals: 'twitter.com'
+    }
+  ]
+};
+chrome.webNavigation.onCommitted.addListener(onWebNav, filter);
+chrome.webNavigation.onHistoryStateUpdated.addListener(onWebNav, filter);
